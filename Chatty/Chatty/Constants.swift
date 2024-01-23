5d7b8a3e11cc4952be4a931462a7e6936458f1b9
Jules <ju.pabst@gmail.com>
2024-01-23 17:41:52 +0100

    added more strings to constants file and added FStore struct to constants struct

--- Chatty/Chatty/Constants.swift
+++ Chatty/Chatty/Constants.swift
@@ -6,6 +6,15 @@
 //
 
 struct Constants {
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
     static let registerSegue = "RegisterToChat";
     static let logInSegue = "LogInToChat";
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
 }
