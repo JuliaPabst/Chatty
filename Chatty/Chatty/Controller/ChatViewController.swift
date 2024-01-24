120fe04487283565d32cfb8932b0f5218671d904
Jules <ju.pabst@gmail.com>
2024-01-24 17:28:52 +0100

    added numberOfRowsInSection function and cellForRowAt function to UITableVIewDataSource extension in ChatViewController

--- Chatty/Chatty/Controller/ChatViewController.swift
+++ Chatty/Chatty/Controller/ChatViewController.swift
@@ -39,11 +39,11 @@ class ChatViewController: UIViewController {
 
 extension ChatViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return messages.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: IndexPath)
     }
     
     
