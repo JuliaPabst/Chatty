0b6bffd551f9f9facfe83466de8010979cf65619
Jules <ju.pabst@gmail.com>
2024-01-26 18:38:21 +0100

    disabled AutoToolBar of IQKeyboardManager and set resignOnTouchOutside to true

--- Chatty/Chatty/AppDelegate.swift
+++ Chatty/Chatty/AppDelegate.swift
@@ -18,7 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
         IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.resignOnTouchOutside = true
+        
         let db = Firestore.firestore()
         return true
     }
